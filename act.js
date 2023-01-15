2186fbcca1754dd91db9f4f4dcb452df8feae177
xristina <120731594+xristinax@users.noreply.github.com>
2023-01-15 14:57:45 +0200

    Add files via upload

---
+++ act.js
@@ -0,0 +1,245 @@
+let msg1 = document.querySelector("#message1");
+let button1 = document.querySelector("#button1")
+button1.addEventListener('click', () =>{
+    document.querySelector('#message1').style.visibility = 'visible';
+    document.querySelector('#message2').style.visibility = 'hidden';
+    document.querySelector('#message3').style.visibility = 'hidden';
+    document.querySelector('#message4').style.visibility = 'hidden';
+    document.querySelector('#message5').style.visibility = 'hidden';
+})
+
+let msg2 = document.querySelector("#message2");
+let button2 = document.querySelector("#button2")
+button2.addEventListener('click', () =>{
+    document.querySelector('#message2').style.visibility = 'visible';
+    document.querySelector('#message1').style.visibility = 'hidden';
+    document.querySelector('#message3').style.visibility = 'hidden';
+    document.querySelector('#message4').style.visibility = 'hidden';
+    document.querySelector('#message5').style.visibility = 'hidden';
+})
+
+let msg3 = document.querySelector("#message3");
+let button3 = document.querySelector("#button3")
+button3.addEventListener('click', () =>{
+    document.querySelector('#message3').style.visibility = 'visible';
+    document.querySelector('#message1').style.visibility = 'hidden';
+    document.querySelector('#message2').style.visibility = 'hidden';
+    document.querySelector('#message4').style.visibility = 'hidden';
+    document.querySelector('#message5').style.visibility = 'hidden';
+})
+
+let msg4 = document.querySelector("#message4");
+let button4 = document.querySelector("#button4")
+button4.addEventListener('click', () =>{
+    document.querySelector('#message4').style.visibility = 'visible';
+    document.querySelector('#message1').style.visibility = 'hidden';
+    document.querySelector('#message2').style.visibility = 'hidden';
+    document.querySelector('#message3').style.visibility = 'hidden';
+    document.querySelector('#message5').style.visibility = 'hidden';
+})
+
+let msg5 = document.querySelector("#message5");
+let button5 = document.querySelector("#button5")
+button5.addEventListener('click', () =>{
+    document.querySelector('#message5').style.visibility = 'visible';
+    document.querySelector('#message1').style.visibility = 'hidden';
+    document.querySelector('#message2').style.visibility = 'hidden';
+    document.querySelector('#message3').style.visibility = 'hidden';
+    document.querySelector('#message4').style.visibility = 'hidden';
+})
+
+
+
+let msg11 = document.querySelector("#message11");
+let button11 = document.querySelector("#button11")
+button11.addEventListener('click', () =>{
+    document.querySelector('#message11').style.visibility = 'visible';
+    document.querySelector('#message12').style.visibility = 'hidden';
+    document.querySelector('#message13').style.visibility = 'hidden';
+    document.querySelector('#message15').style.visibility = 'hidden';
+    document.querySelector('#message16').style.visibility = 'hidden';
+    document.querySelector('#message17').style.visibility = 'hidden';
+    document.querySelector('#message18').style.visibility = 'hidden';
+    document.querySelector('#message19').style.visibility = 'hidden';
+    document.querySelector('#message21').style.visibility = 'hidden';
+    document.querySelector('#message22').style.visibility = 'hidden';
+    document.querySelector('#message25').style.visibility = 'hidden';
+
+})
+
+let msg12 = document.querySelector("#message12");
+let button12 = document.querySelector("#button12")
+button12.addEventListener('click', () =>{
+    document.querySelector('#message15').style.visibility = 'hidden';
+    document.querySelector('#message11').style.visibility = 'hidden';
+    document.querySelector('#message12').style.visibility = 'visible';
+    document.querySelector('#message13').style.visibility = 'hidden';
+    document.querySelector('#message16').style.visibility = 'hidden';
+    document.querySelector('#message17').style.visibility = 'hidden';
+    document.querySelector('#message18').style.visibility = 'hidden';
+    document.querySelector('#message19').style.visibility = 'hidden';
+    document.querySelector('#message21').style.visibility = 'hidden';
+    document.querySelector('#message22').style.visibility = 'hidden';
+    document.querySelector('#message25').style.visibility = 'hidden';
+
+})
+
+let msg13 = document.querySelector("#message13");
+let button13 = document.querySelector("#button13")
+button13.addEventListener('click', () =>{
+    document.querySelector('#message13').style.visibility = 'visible';
+    document.querySelector('#message11').style.visibility = 'hidden';
+    document.querySelector('#message12').style.visibility = 'hidden';
+    document.querySelector('#message15').style.visibility = 'hidden';
+    document.querySelector('#message16').style.visibility = 'hidden';
+    document.querySelector('#message17').style.visibility = 'hidden';
+    document.querySelector('#message18').style.visibility = 'hidden';
+    document.querySelector('#message19').style.visibility = 'hidden';
+    document.querySelector('#message21').style.visibility = 'hidden';
+    document.querySelector('#message22').style.visibility = 'hidden';
+    document.querySelector('#message25').style.visibility = 'hidden';
+
+})
+
+let msg15 = document.querySelector("#message15");
+let button15 = document.querySelector("#button15")
+button15.addEventListener('click', () =>{
+    document.querySelector('#message15').style.visibility = 'visible';
+    document.querySelector('#message11').style.visibility = 'hidden';
+    document.querySelector('#message12').style.visibility = 'hidden';
+    document.querySelector('#message13').style.visibility = 'hidden';
+    document.querySelector('#message16').style.visibility = 'hidden';
+    document.querySelector('#message17').style.visibility = 'hidden';
+    document.querySelector('#message18').style.visibility = 'hidden';
+    document.querySelector('#message19').style.visibility = 'hidden';
+    document.querySelector('#message21').style.visibility = 'hidden';
+    document.querySelector('#message22').style.visibility = 'hidden';
+    document.querySelector('#message25').style.visibility = 'hidden';
+
+})
+
+let msg16 = document.querySelector("#message16");
+let button16 = document.querySelector("#button16")
+button16.addEventListener('click', () =>{
+    document.querySelector('#message16').style.visibility = 'visible';
+    document.querySelector('#message11').style.visibility = 'hidden';
+    document.querySelector('#message12').style.visibility = 'hidden';
+    document.querySelector('#message13').style.visibility = 'hidden';
+    document.querySelector('#message15').style.visibility = 'hidden';
+    document.querySelector('#message17').style.visibility = 'hidden';
+    document.querySelector('#message18').style.visibility = 'hidden';
+    document.querySelector('#message19').style.visibility = 'hidden';
+    document.querySelector('#message21').style.visibility = 'hidden';
+    document.querySelector('#message22').style.visibility = 'hidden';
+    document.querySelector('#message25').style.visibility = 'hidden';
+
+})
+
+let msg17 = document.querySelector("#message17");
+let button17 = document.querySelector("#button17")
+button17.addEventListener('click', () =>{
+    document.querySelector('#message17').style.visibility = 'visible';
+    document.querySelector('#message11').style.visibility = 'hidden';
+    document.querySelector('#message12').style.visibility = 'hidden';
+    document.querySelector('#message13').style.visibility = 'hidden';
+    document.querySelector('#message15').style.visibility = 'hidden';
+    document.querySelector('#message16').style.visibility = 'hidden';
+    document.querySelector('#message18').style.visibility = 'hidden';
+    document.querySelector('#message19').style.visibility = 'hidden';
+    document.querySelector('#message21').style.visibility = 'hidden';
+    document.querySelector('#message22').style.visibility = 'hidden';
+    document.querySelector('#message25').style.visibility = 'hidden';
+
+})
+
+let msg18 = document.querySelector("#message18");
+let button18 = document.querySelector("#button18")
+button18.addEventListener('click', () =>{
+    document.querySelector('#message18').style.visibility = 'visible';
+    document.querySelector('#message11').style.visibility = 'hidden';
+    document.querySelector('#message12').style.visibility = 'hidden';
+    document.querySelector('#message13').style.visibility = 'hidden';
+    document.querySelector('#message15').style.visibility = 'hidden';
+    document.querySelector('#message16').style.visibility = 'hidden';
+    document.querySelector('#message17').style.visibility = 'hidden';
+    document.querySelector('#message19').style.visibility = 'hidden';
+    document.querySelector('#message21').style.visibility = 'hidden';
+    document.querySelector('#message22').style.visibility = 'hidden';
+    document.querySelector('#message25').style.visibility = 'hidden';
+
+})
+
+let msg19 = document.querySelector("#message19");
+let button19 = document.querySelector("#button19")
+button19.addEventListener('click', () =>{
+    document.querySelector('#message19').style.visibility = 'visible';
+    document.querySelector('#message11').style.visibility = 'hidden';
+    document.querySelector('#message12').style.visibility = 'hidden';
+    document.querySelector('#message13').style.visibility = 'hidden';
+    document.querySelector('#message15').style.visibility = 'hidden';
+    document.querySelector('#message16').style.visibility = 'hidden';
+    document.querySelector('#message17').style.visibility = 'hidden';
+    document.querySelector('#message18').style.visibility = 'hidden';
+    document.querySelector('#message21').style.visibility = 'hidden';
+    document.querySelector('#message22').style.visibility = 'hidden';
+    document.querySelector('#message25').style.visibility = 'hidden';
+})
+
+let msg21 = document.querySelector("#message21");
+let button21 = document.querySelector("#button21")
+button21.addEventListener('click', () =>{
+    document.querySelector('#message21').style.visibility = 'visible';
+    document.querySelector('#message11').style.visibility = 'hidden';
+    document.querySelector('#message12').style.visibility = 'hidden';
+    document.querySelector('#message13').style.visibility = 'hidden';
+    document.querySelector('#message15').style.visibility = 'hidden';
+    document.querySelector('#message16').style.visibility = 'hidden';
+    document.querySelector('#message17').style.visibility = 'hidden';
+    document.querySelector('#message18').style.visibility = 'hidden';
+    document.querySelector('#message19').style.visibility = 'hidden';
+    document.querySelector('#message22').style.visibility = 'hidden';
+    document.querySelector('#message25').style.visibility = 'hidden';
+
+})
+
+let msg22 = document.querySelector("#message22");
+let button22 = document.querySelector("#button22")
+button22.addEventListener('click', () =>{
+    document.querySelector('#message22').style.visibility = 'visible';
+    document.querySelector('#message11').style.visibility = 'hidden';
+    document.querySelector('#message12').style.visibility = 'hidden';
+    document.querySelector('#message13').style.visibility = 'hidden';
+    document.querySelector('#message15').style.visibility = 'hidden';
+    document.querySelector('#message16').style.visibility = 'hidden';
+    document.querySelector('#message17').style.visibility = 'hidden';
+    document.querySelector('#message18').style.visibility = 'hidden';
+    document.querySelector('#message19').style.visibility = 'hidden';
+    document.querySelector('#message21').style.visibility = 'hidden';
+    document.querySelector('#message25').style.visibility = 'hidden';
+})
+
+
+
+let msg25 = document.querySelector("#message25");
+let button25 = document.querySelector("#button25")
+button25.addEventListener('click', () =>{
+    document.querySelector('#message25').style.visibility = 'visible';
+    document.querySelector('#message11').style.visibility = 'hidden';
+    document.querySelector('#message12').style.visibility = 'hidden';
+    document.querySelector('#message13').style.visibility = 'hidden';
+    document.querySelector('#message15').style.visibility = 'hidden';
+    document.querySelector('#message16').style.visibility = 'hidden';
+    document.querySelector('#message17').style.visibility = 'hidden';
+    document.querySelector('#message18').style.visibility = 'hidden';
+    document.querySelector('#message19').style.visibility = 'hidden';
+    document.querySelector('#message21').style.visibility = 'hidden';
+    document.querySelector('#message22').style.visibility = 'hidden';
+})
+
+
+
+
+
+
+
+
